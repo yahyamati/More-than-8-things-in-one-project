c3e4b3f7663a7cbe5c4e2a0d116005ad765b1545
Yahia Mati <yahyamati8@gmail.com>
2025-02-14 17:05:59 +0100

    create a server HONO

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,6 +1,14 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
-//...
+app.use('*',logger())
+
+app.get('/test', c => {
+    return c.json({ message: 'test' })
+})
+app.get('/notfound' , (c)=>{
+    return c.notFound()
+})
 
 export default app // for Cloudflare Workers or Bun
--- Backend/index.ts
+++ Backend/index.ts
@@ -1,10 +1,12 @@
+import app from "./app";
+
+
 Bun.serve({
     port: 3000,//authomatically set to 3000 if not provided 
     hostname: "0.0.0.0",
-    fetch(req) {
-      return new Response("Hello from Bun server!");
+    fetch: app.fetch
     },
-  });
+  );
 
 
 console.log("server is running");
