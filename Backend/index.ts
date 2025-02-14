61c57d05765ee3bd3ec9b7aece5ec012a03460a6
Yahia Mati <yahyamati8@gmail.com>
2025-02-14 16:39:51 +0100

    install hono a Web framework built on Web Standards for Cloudflare Workers

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!?");
+Bun.serve({
+    port: 3000,//authomatically set to 3000 if not provided 
+    hostname: "0.0.0.0",
+    fetch(req) {
+      return new Response("Hello from Bun server!");
+    },
+  });
+
+
+console.log("server is running");
