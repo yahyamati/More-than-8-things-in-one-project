131ae196199825a51c079bb97ab0c5cf4386f979
Yahia Mati <yahyamati8@gmail.com>
2025-02-16 12:29:47 +0100

    create page work good

--- Frontend/src/routes/create-expenses.tsx
+++ Frontend/src/routes/create-expenses.tsx
@@ -1,10 +1,131 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute , useNavigate  } from '@tanstack/react-router'
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label";
+import { Button } from "../components/ui/button";
+// import { Calendar } from "../components/ui/calendar";
+// import { toast } from "sonner"
+import { useForm } from "@tanstack/react-form";
+import {api} from "../lib/api.ts"
 
 export const Route = createFileRoute('/expenses' as never)({
   component: CreateExpenses,
 })
 
+
+
+
 function CreateExpenses() {
- 
-  return <div>Create Expenses</div>
+  const navigate = useNavigate();
+
+
+
+
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      amount:0
+    },
+    onSubmit: async ({ value }) => {
+      
+      const res = await api.expenses.$post({ json: value });
+      if (!res.ok) {
+        throw new Error("server error");
+      }else{
+        navigate({to :"/expenses" as never});
+      }
+      
+    },
+  })
+  return (
+    <div className="p-2">
+      <h2>Create Expense</h2>
+      
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
+          }}
+          className="flex flex-col gap-y-4 max-w-xl m-auto"
+        >
+          <form.Field
+            name="title"
+            // validators={{
+            //   onChange: createExpenseSchema.shape.title,
+            // }}
+            children={(field) => (
+              <div>
+                <Label htmlFor={field.name}>Title</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange((e.target.value))}
+                />
+                {field.state.meta.isTouched && field.state.meta.errors.length ? (
+                <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+              </div>
+            )}
+          />
+
+          <form.Field
+            name="amount"
+            // validators={{
+            //   onChange: createExpenseSchema.shape.amount,
+            // }}
+            children={(field) => (
+              <div>
+                <Label htmlFor={field.name}>Amount</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  type="number"
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                 {field.state.meta.isTouched && field.state.meta.errors.length ? (
+                <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+              </div>
+            )}
+          />
+
+          {/* <form.Field
+            name="date"
+            // validators={{
+            //   onChange: createExpenseSchema.shape.date,
+            // }}
+            children={(field) => (
+              <div className="self-center">
+                <Calendar
+                  mode="single"
+                  selected={new Date(field.state.value)}
+                  onSelect={(date) =>
+                    field.handleChange((date ?? new Date()).toISOString())
+                  }
+                  className="rounded-md border"
+                />
+                {field.state.meta.isTouched && field.state.meta.errors.length ? (
+                <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+              </div>
+            )}
+          /> */}
+
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (
+              <Button className="mt-4" type="submit" disabled={!canSubmit}>
+                {isSubmitting ? "..." : "Submit"}
+              </Button>
+            )}
+          />
+        </form>
+     
+    </div>
+  );
 }
+  
